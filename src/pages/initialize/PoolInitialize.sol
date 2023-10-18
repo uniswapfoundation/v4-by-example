0d403cfdd61753b7dd0bc7e0ccff81825de0785a
saucepoint <saucepoint@protonmail.com>
2023-10-18 13:55:11 -0400

    first pass on pool initialization

---
+++ src/pages/initialize/PoolInitialize.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "@uniswap/v4-core/contracts/types/Currency.sol";
+
+contract PoolInitialize {
+    using CurrencyLibrary for Currency;
+
+    // set the pool manager address
+    IPoolManager manager = IPoolManager(0x01);
+
+    function init(
+        address token0,
+        address token1,
+        uint24 swapFee,
+        int24 tickSpacing,
+        address hook,
+        uint160 sqrtPriceX96,
+        bytes calldata hookData
+    ) external {
+        // sort your tokens! v4 expects: token0 < token1
+        if (token0 > token1) {
+            (token0, token1) = (token1, token0);
+        }
+
+        PoolKey memory pool = PoolKey({
+            currency0: Currency(token0),
+            currency1: Currency(token1),
+            fee: swapFee,
+            tickSpacing: tickSpacing,
+            hooks: IHooks(hook)
+        });
+        manager.initialize(pool, sqrtPriceX96, hookData);
+    }
+}
