03cf75469d348c5f50ce253fb86d4fc1cad1aa97
saucepoint <saucepoint@protonmail.com>
2024-03-05 15:46:27 -0500

    add periphery for BaseHook

--- src/pages/fees/dynamic-fee/AutoDynamicFee.sol
+++ src/pages/fees/dynamic-fee/AutoDynamicFee.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// TODO: update to v4-periphery/BaseHook.sol when its compatible
-import {BaseHook} from "@v4-by-example/utils/BaseHook.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
 
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
