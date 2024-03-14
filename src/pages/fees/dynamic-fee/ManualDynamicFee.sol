ed472a569051a594f6ea9275a5d20930176ec516
saucepoint <saucepoint@protonmail.com>
2024-03-14 12:48:48 +0000

    update dynamic fees

--- src/pages/fees/dynamic-fee/ManualDynamicFee.sol
+++ src/pages/fees/dynamic-fee/ManualDynamicFee.sol
@@ -6,10 +6,9 @@ import {BaseHook} from "v4-periphery/BaseHook.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
-import {IDynamicFeeManager} from "v4-core/interfaces/IDynamicFeeManager.sol";
 
 /// @notice A time-decaying dynamically fee, updated manually with external PoolManager.updateDynamicSwapFee() calls
-contract ManualDynamicFee is BaseHook, IDynamicFeeManager {
+contract ManualDynamicFee is BaseHook {
     uint256 public immutable startTimestamp;
 
     // Start at 5% fee, decaying at rate of 0.00001% per second
@@ -24,21 +23,32 @@ contract ManualDynamicFee is BaseHook, IDynamicFeeManager {
         startTimestamp = block.timestamp;
     }
 
-    /// @dev Deteremines a Pool's swap fee. Called and cached by PoolManager.updateDynamicFee()
-    function getFee(address, PoolKey calldata) external view override returns (uint24 _currentFee) {
+    /// @dev Deteremines a Pool's swap fee
+    function setFee(PoolKey calldata key) public {
         // Linearly decaying fee, y = mx + b
         // After 495,000 seconds (5.72 days), fee will be a minimum of 0.05%
+        uint24 _currentFee;
         unchecked {
             uint256 timeElapsed = block.timestamp - startTimestamp;
             _currentFee = timeElapsed > 495000 ? uint24(MIN_FEE) : uint24((START_FEE - (timeElapsed * decayRate)) / 10);
         }
+        poolManager.updateDynamicSwapFee(key, _currentFee);
+    }
+
+    function afterInitialize(address, PoolKey calldata key, uint160, int24, bytes calldata)
+        external
+        override
+        returns (bytes4)
+    {
+        setFee(key);
+        return BaseHook.afterInitialize.selector;
     }
 
     /// @dev this example hook contract does not implement any hooks
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
-            afterInitialize: false,
+            afterInitialize: true,
             beforeAddLiquidity: false,
             afterAddLiquidity: false,
             beforeRemoveLiquidity: false,
